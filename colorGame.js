5037cb515db8e0144f3d30c0641fc9502771b6c6
Asuni Oluwaseun <asunioluwaseun@gmail.com>
2020-02-06 13:10:39 +0100

    wrongly selected squares disapears

--- colorGame.js
+++ colorGame.js
@@ -13,6 +13,7 @@ var colors = [
 var squares = document.querySelectorAll(".square");
 var pickedColor = colors[3];
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
@@ -28,7 +29,8 @@ for(var i = 0; i < squares.length; i++){
         if(clickedColor === pickedColor){
             alert("correct");
         } else {
-            alert("WRONG!!!");
+            this.style.backgroundColor = "#232323";
+            messageDisplay.textContent = "Try Again!";
         }
     });
 }
